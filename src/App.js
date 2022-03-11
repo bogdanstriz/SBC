a70227feb4af571264217e37e85049e3ca502ad1
Bogdan Strizheus <bogdanstriz@gmail.com>
2022-03-10 23:32:10 -0500

    firebase

--- src/App.js
+++ src/App.js
@@ -10,6 +10,27 @@ import 'aos/dist/aos.css';
 
 import 'scss/react-images.scss';
 import 'scss/slick-slider.scss';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB0C9suhziNgOEqE0Uk1ufZI7mt3GoNpt8",
+  authDomain: "slavicbethelchurch-81e42.firebaseapp.com",
+  projectId: "slavicbethelchurch-81e42",
+  storageBucket: "slavicbethelchurch-81e42.appspot.com",
+  messagingSenderId: "494972932316",
+  appId: "1:494972932316:web:0f0ca6d96a00ee35372419",
+  measurementId: "G-SCQZ0TYTDN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const browserHistory = createBrowserHistory();
 
